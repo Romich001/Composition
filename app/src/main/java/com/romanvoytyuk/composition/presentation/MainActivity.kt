2468f8c491875e97ebf1a0a89fb7229789f7012f
Roman Voytyuk <romavoytyuk@gmail.com>
2023-06-06 09:04:34 +0300

    init commit

---
+++ app/src/main/java/com/romanvoytyuk/composition/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.romanvoytyuk.composition.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.romanvoytyuk.composition.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
